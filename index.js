d2c6479845aa219dfc7298168c5ad6cc4199aa3d
Angelo Gabrieli <47817899+agabriel81@users.noreply.github.com>
2019-08-13 12:28:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var hostname = os.hostname();
 const fs = require('fs')
 
 const https = require('https')
-const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello HTTPS from ' + hostname)
