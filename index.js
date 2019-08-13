03e774aaaed2adfc1e1a1581c1fac6f38280001a
Angelo Gabrieli <47817899+agabriel81@users.noreply.github.com>
2019-08-13 09:32:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var os = require('os');
-os.hostname();
+var os = require("os");
+var hostname = os.hostname();
 
 const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World from "os.hostname()"!");
+    response.end("Hello World from " + hostname);
 });
 
 const port = process.env.PORT || 8080;
