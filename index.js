87e5abe1762d1a8f6c984e442785596eb06832ec
Angelo Gabrieli <47817899+agabriel81@users.noreply.github.com>
2020-11-04 12:34:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-var https2 = require('https2');
-var os = require("os");
-var hostname = os.hostname();
-var https = require('https');
-var fs = require('fs');
-var https_options = {
+const http2 = require('http2');
+const options = {
   key: fs.readFileSync("/opt/app-root/certificate/tls.key"),
   cert: fs.readFileSync("/opt/app-root/certificate/tls.crt"),
   ca: [
@@ -11,9 +7,10 @@ var https_options = {
           fs.readFileSync('/opt/app-root/certificate/tls.crt')
        ]
 };
- 
-https2.createServer(https_options, function (req, res) {
- res.writeHead(200);
- res.end("Welcome to Node.js HTTPS Server : " + hostname);
 
-}).listen(8443)
+// https is necessary otherwise browsers will not
+// be able to connect
+const server = http2.createSecureServer(options, (req, res) => {
+ res.end('Hello World!');
+});
+server.listen(8443);
